05fc737c2c432e23ae6ccb54fb01657b4b0c277f
jaivi931-cmd <imjaivi931@gmail.com>
2026-09-17 08:31:35 +0400

    Update app.py

--- app.py
+++ app.py
@@ -151,6 +151,6 @@ def merge_video():
     except Exception as e:
         return jsonify({'error': f"Video merging failed: {str(e)} "}), 500
 
-if __name__ == '---main__':
+if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
